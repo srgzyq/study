63d7c81893f019e32cad72651e8c138579ead48e
shirui_mac <srgzyq@playcrab.com>
2012-09-16 20:48:47 +0800

    factory

---
+++ DesignPattern/factory/factoryfm/src/Pizza.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public abstract class Pizza {
+    // 名称 面团类型 酱料类型 一套佐料
+    String name; 
+    String dough;
+    String sauce;
+    ArrayList toppings = new ArrayList();
+
+    void prepare() {
+        System.out.println("Preparing " + name);
+        System.out.println("Tossing dough...");
+        System.out.println("Adding toppings: ");
+        for(int i = 0; i < toppings.size(); i++) {
+            System.out.println("    " + toppings.get(i));
+        }
+    }
+    
+    void bake() {
+        System.out.println("Bake for 25 minutes at 350");
+    }
+
+    void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");
+    }
+
+    void box() {
+        System.out.println("Place pizza in official");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
