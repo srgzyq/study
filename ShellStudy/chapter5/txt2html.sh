719c012cec24099037eb6bd47efa2998de396c39
shirui_mac <srgzyq@playcrab.com>
2012-12-02 17:27:27 +0800

    add shell

---
+++ ShellStudy/chapter5/txt2html.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is a simple script that you can use for converting text into HTML.
+# First we take out all newline characters, so that the appending only happens
+# once, the we replace the newlines.
+
+echo "converting $1..."
+
+SCRIPT="./script.sed"
+NAME="$1"
+TEMPFILE="/var/tmp/sed.$PID.tmp"
+sed "s/\n/^M/" $1 | sed -f $script | sed "s/^M/\n/" > $TEMPFILE
+mv $TEMPFILE $NAME
+
+echo "done."
+
