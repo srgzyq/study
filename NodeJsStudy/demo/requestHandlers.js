a4473c2216ac5789cb0e5889308884077cbd0bc0
shirui_mac <srgzyq@playcrab.com>
2012-12-04 23:39:16 +0800

    node js

--- NodeJsStudy/demo/requestHandlers.js
+++ NodeJsStudy/demo/requestHandlers.js
@@ -1,26 +1,41 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring")
 
-function start() {
+function start(response,postData) {
     console.log("Request handler 'start' was called.");
-    /*function sleep(milliSeconds) {
-        var startTime = new Date().getTime();
-        while(new Date().getTime() < startTime + milliSeconds);
-    }
 
-    sleep(10000);
-    return "Hello Start";
-    */
-    var content = "empty";
-
-    exec("ls -lah", function (error, stdout,stderr) {
-        content = stdout
+    /*exec("ls -lah", function (error, stdout,stderr) {
+        response.writeHead(200,{"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
     });
-    return content
+    */
+
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</from>'+
+        '</body>'+
+        '</html>';
+
+        response.writeHead(200,{"Content-Type": "text/html"});
+        response.write(body);
+        response.end();
 }
 
-function upload() {
+function upload(response,postData) {
     console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
+    response.writeHead(200,{"Content-Type": "text/plain"});
+    //response.write("You've sent:" + postData);
+    response.write("You've sent the text: "+
+    querystring.parse(postData).text);
+    response.end();
 }
 
 exports.start = start;
