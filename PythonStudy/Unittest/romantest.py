dacbf9c98653b9694e67a814a878a608e31c76b0
shirui_mac <srgzyq@playcrab.com>
2013-01-29 09:26:38 +0800

    add

--- PythonStudy/Unittest/romantest.py
+++ PythonStudy/Unittest/romantest.py
@@ -90,3 +90,18 @@ class ToRomanBadInput(unittest.TestCase):
         """toRoman should fail with non-integer input"""
         self.assertRaises(roman.NotIntegerError,roman.toRoman,0.5)
 
+class FromRomanBadInput(unittest.TestCase):
+    def testTooManyRepeatedNumerals(self):
+        """ fromRoman should fail with too many repeated numerals """
+        for s in ('MMMM', 'DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII')
+            self.assertRaises(roman.InvalidRomanNumeralError,roman.fromRoman,s)
+
+    def testRepeatedPairs(self):
+        """ fromRoman should fail with repeated pairs of numerals """
+        for s in ('CMCM', 'CDCD', 'XCXC', 'XLXL', 'IXIX', 'IVIV'):
+            self.assertRaises(roman.InvalidRomanNumeralError,roman.fromRoman,s)
+
+    def testMalformedAntecedent(self):
+        """fromRoman should fail with malformed antecedents"""
+        for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV', 'MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
+            self.assertRaises(roman.InvalidRomanNumeralError,roman.fromRoman,s)
