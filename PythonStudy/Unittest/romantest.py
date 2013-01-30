83afe1c7e53a3b18b100b2132de9bcefffe8782b
shirui_mac <srgzyq@playcrab.com>
2013-01-30 08:28:15 +0800

    add test

--- PythonStudy/Unittest/romantest.py
+++ PythonStudy/Unittest/romantest.py
@@ -105,3 +105,27 @@ class FromRomanBadInput(unittest.TestCase):
         """fromRoman should fail with malformed antecedents"""
         for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV', 'MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
             self.assertRaises(roman.InvalidRomanNumeralError,roman.fromRoman,s)
+
+class SanityCheck(unittest.TestCase):
+    def testSanity(self):
+        """fromRoman(toRoman(n)) == n for all n"""
+        for integer in range(1, 4000):
+            numeral = roman.toRoman(integer)
+            result = roman.fromRoman(numeral)
+            self.assertRaises(integer,result)
+
+class CaseCheck(unittest.TestCase):
+    def testToRomanCase(self):
+        """toRoman should always return uppercase"""
+        for integer in range(1,4000):
+            numeral = roman.toRoman(integer)
+            self.assertEqual(numeral,numeral.upper())
+
+    def testFromRomanCase(self):
+        """fromRoman should only accept uppercase input"""
+        for integer in range(1,4000):
+            numeral = roman.toRoman(integer)
+            roman.fromRoman(numeral.upper())
+            self.assertRaises(roman.InvalidRomanNumeralError,roman.fromRoman,numeral.lower())
+
+
