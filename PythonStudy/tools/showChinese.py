445a80f8afbe422ec13e923708e65ce14c8e1e60
shirui_mac <srgzyq@playcrab.com>
2013-03-30 12:50:55 +0800

    showChines

---
+++ PythonStudy/tools/showChinese.py
@@ -0,0 +1,5 @@
+import urllib
+
+filePath="/Develop/ares/svn/Assets.zh_TW/anim/ui/%E5%AF%B9%E8%AF%9D%E6%A1%86/%E6%B1%9F%E6%B9%96%E6%88%98%E6%96%97%E5%90%8E%E5%BC%9F%E5%AD%90%E6%95%B0%E6%8D%AE/ui_JiangHuWanJiaDiZiShuJu.psd"
+
+print urllib.unquote(filePath).decode("utf-8")
