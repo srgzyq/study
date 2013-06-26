b1dfd87590b91b938809bda94531fd543918653c
shirui_mac <srgzyq@playcrab.com>
2013-06-27 07:20:15 +0800

    add

--- ShellStudy/chapter5_sed/txt2html.sh
+++ ShellStudy/chapter5_sed/txt2html.sh
@@ -8,9 +8,9 @@ echo "converting $1..."
 
 SCRIPT="./script.sed"
 NAME="$1"
-TEMPFILE="/var/tmp/sed.$PID.tmp"
-sed "s/\n/^M/" $1 | sed -f $script | sed "s/^M/\n/" > $TEMPFILE
-mv $TEMPFILE $NAME
+TEMPFILE="./test1"
+sed "s/\n/^M/" $1 | sed -f $SCRIPT | sed "s/^M/\n/" > $TEMPFILE
+#mv $TEMPFILE $NAME
 
 echo "done."
 
