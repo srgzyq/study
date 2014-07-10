f9e2b677e4825f601a619bf47f4775cc60943954
srgzyq_air <srgzyq@playcrab.com>
2014-07-10 09:44:45 +0800

    24

---
+++ ruby/ex24.rb
@@ -0,0 +1,13 @@
+puts "Let's practice everythin."
+puts "You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs."
+
+poem = <<MULTI_LINE_STRING
+
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+
+MULTI_LINE_STRING
