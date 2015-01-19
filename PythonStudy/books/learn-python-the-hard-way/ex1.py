fd6e5ab9840a56c70cb5ab1cc4119505785ba351
srgzyq_air <srgzyq@playcrab.com>
2015-01-19 18:29:19 +0800

    add

--- PythonStudy/books/learn-python-the-hard-way/ex1.py
+++ PythonStudy/books/learn-python-the-hard-way/ex1.py
@@ -1,7 +1,9 @@
-print "Hello World!"
+# _*_ coding: utf-8 _*_
+print "Hello World! Beijing hello!"
 print "Hello Again"
 print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+print "你好"
