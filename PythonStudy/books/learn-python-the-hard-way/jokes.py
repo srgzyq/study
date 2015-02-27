06bdab4a3fa44c0c42bb31eb741378ee972bae36
srgzyq_air <srgzyq@playcrab.com>
2015-02-27 11:41:29 +0800

    add

---
+++ PythonStudy/books/learn-python-the-hard-way/jokes.py
@@ -0,0 +1,18 @@
+print('What do you get when you cross a snowman with a vamprie?')
+input()
+print('Frostbite!')
+print()
+print('What do detists call a astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
+print('Knock knock.')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
+
+
