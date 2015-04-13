33901b1b8f4193c0850f2c1a970fe2333b3aa9c8
srgzyq_air <srgzyq@playcrab.com>
2015-04-13 20:49:45 +0800

    add

--- PythonStudy/mysite/mysite/books/views.py
+++ PythonStudy/mysite/mysite/books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from mysite.books.models import Book
+from mysite.books.forms import ContactForm 
 from django.core.mail import send_mail
 
 # Create your views here.
@@ -38,7 +39,40 @@ def search(request):
     return render_to_response('search_form.html', {'errors':errors})
 
 
+'''
 def contact(request):
     errors = []
     if request.method == 'POST':
+        if not request.POST.get('subject',''):
+            errors.append('Enter a subject.')
+        if not request.POST.get('message',''):
+            errors.append('Enter a message.')
+        if request.POST.get('email') and '@' not in request.POST['email']:
+            errors.append('Enter a valid e-mail address.')
+        if not errors:
+            send_mail(
+                request.POST['subject'],
+                request.POST['message'],
+                request.POST.get('email','noreply@example.com'),
+                ['siteowner@example.com'],
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    return render(request, 'contact_form.html',{'errors':errors})
     return render_to_response('contact_form.html', {'errors': errors})
+'''
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email','noreply@example.com'),
+                ['siteowner@example.com'],
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm()
+    return render_to_response('contact_form_1.html', {'form': form})
--- PythonStudy/mysite/mysite/settings.py
+++ PythonStudy/mysite/mysite/settings.py
@@ -87,3 +87,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = '472681814@qq.com'
+EMAIL_HOST_PASSWORD = 'sr870830'
