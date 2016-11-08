0fe0dcf6d523505eafc32cdf2beec14d590fa316
srgzyq <srgzyq@gmail.com>
2016-11-08 17:58:45 +0800

    change name

--- PythonStudy/tools/luafile.lua
+++ PythonStudy/tools/luafile.lua
@@ -1,1 +1,1 @@
-a={["srgzyq"]={["person"]={[0]={["key"]="test",["value"]=2000,},[1]={["key"]="playcrab",["value"]=1234,},},["age"]=12,["name"]="srgzyq",},}
+a={["srgzyq"]={["person"]={[0]={["key"]="test",["value"]=2000,},[1]={["key"]="jijia",["value"]=1234,},},["age"]=12,["name"]="hello",},}
